01be8b00c21543c90d80afa8cf210de05718a57b
Razumov Pavel <razumov_pv@mail.ru>
2025-07-09 14:28:54 +0300

    Initial commit

--- Documents/DocumentsViewController.swift
+++ Documents/DocumentsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DocumentsViewController.swift
 //  Documents
 //
 //  Created by Razumov Pavel on 09.07.2025.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class DocumentsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
     }
 
-
 }
-
